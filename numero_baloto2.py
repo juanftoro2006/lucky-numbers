4c313ee80dbcbbee93a26dc59c808bed1904c0cc
Juan Toro <juanf.toroi@gmail.com>
2025-03-31 00:33:18 -0500

    agregado 31/03/25 - 12:33

--- numero_baloto2.py
+++ numero_baloto2.py
@@ -1,23 +1,23 @@
 import tkinter as tk
 import random
 
-
-class GeneradorNumerosAleatorios:
+#aqui se generan 5 numeros aleatorios entre el rango que se dictará
+class GeneradorNumerosAleatorios:      
     def __init__(self, minimo, maximo, cantidad):
         self.minimo = minimo
         self.maximo = maximo
         self.cantidad = cantidad
 
     def generar_numeros(self):
-        return random.sample(range(self.minimo, self.maximo+1), self.cantidad)
+        return random.sample(range(self.minimo, self.maximo+1), self.cantidad)      # se usa el random.sample() para obtener  una lista de números únicos.
 
-class GeneradorNumeroBalota:
+class GeneradorNumeroBalota:        #Esta clase genera la balota roja que es del 1 al 16
     def __init__(self, minim, maxim):
         self.minim = minim
         self.maxim = maxim
 
     def generate_numbers(self):
-        return random.randint(self.minim, self.maxim)
+        return random.randint(self.minim, self.maxim)     # igual con el random...pero este es random.radint que genera 1 número leatorio
 
 # Función para actualizar la interfaz
 def generar_numeros():
@@ -43,20 +43,20 @@ def mostrar_numeros(numeros, balota_roja):
     canvas.create_oval(x, 50, x+50, 100, fill="red", outline="black")
     canvas.create_text(x+25, 75, text=f"{balota_roja:02d}", font=("Arial", 14, "bold"), fill="white")
 
-# Crear instancias de las clases
+# AQUI SE CREAN LAS INSTANCIAS
 generador = GeneradorNumerosAleatorios(1, 43, 5)
 generate = GeneradorNumeroBalota(1, 16)
 
 # Configurar ventana
 root = tk.Tk()
-root.title("Generador de Números")
+root.title("GENERADOR DE LOS NUMEROS GANADORES DE JUAN TORO")
 
 # Crear canvas para dibujar
 canvas = tk.Canvas(root, width=500, height=150)
 canvas.pack()
 
 # Botón para generar números
-btn_generar = tk.Button(root, text="Generar Números", command=generar_numeros)
+btn_generar = tk.Button(root, text="GENERANDO LOS NUMEROS GANADORES", command=generar_numeros, bd=5)
 btn_generar.pack()
 
 # Mostrar números iniciales
